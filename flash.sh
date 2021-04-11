afd10c8eadfe885abd0f2bb56715c3badfaf80d7
Stefano <stefano.stoduto@gmail.com>
2021-04-11 10:12:34 +0100

    better flash script

--- flash.sh
+++ flash.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
 
-arduino-cli compile -v -b arduino:avr:nano "$1" || exit 1
-arduino-cli upload -v -p /dev/ttyUSB0 -b arduino:avr:nano:cpu=atmega328old -t "$1" || exit 2
+COMPILE=0
+UPLOAD=0
+
+function usage() {
+    echo "usage:"
+    echo "  $0 [-c] [-u] [-h]"
+    echo "    -h show this help and exit"
+    echo "    -c compile"
+    echo "    -u upload"
+    echo "witho no parameters defaults to -c -u"
+}
+
+while getopts 'cuh' flag
+do
+    case "${flag}" in
+        c) COMPILE=1
+           shift 1;;
+        u) UPLOAD=1
+           shift 1;;
+        h) usage
+           exit 0 ;;
+        *) usage
+           exit 1 ;;
+    esac
+done
+
+if (( $COMPILE == 0 && $UPLOAD == 0 ))
+then
+    COMPILE=1
+    UPLOAD=1
+fi
+
+if (( $COMPILE == 1 ))
+then
+    arduino-cli compile -v -b arduino:avr:nano "$1" || exit 1
+fi
+
+if (( $UPLOAD == 1 ))
+then
+    arduino-cli upload -v -p /dev/ttyUSB0 -b arduino:avr:nano:cpu=atmega328old -t "$1" && screen /dev/ttyUSB0 9600 || exit 2
+fi
