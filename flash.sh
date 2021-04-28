0b922c25169d75e5c9365f925c79b4836f0d0c79
Stefano <stefano.stoduto@gmail.com>
2021-04-28 20:29:24 +0200

    add option to flash.sh to launch only the serial monitor

--- flash.sh
+++ flash.sh
@@ -2,22 +2,26 @@
 
 COMPILE=0
 UPLOAD=0
+SCREEN=0
 
 function usage() {
     echo "usage:"
-    echo "  $0 [-c] [-u] [-h]"
+    echo "  $0 [-c] [-u] [-s] [-h]"
     echo "    -h show this help and exit"
     echo "    -c compile"
     echo "    -u upload"
-    echo "witho no parameters defaults to -c -u"
+    echo "    -s run screen"
+    echo "witho no parameters defaults to -c -u -s"
 }
 
-while getopts 'cuh' flag
+while getopts 'cush' flag
 do
     case "${flag}" in
         c) COMPILE=1
            shift 1;;
         u) UPLOAD=1
+           shift 1;;
+        s) SCREEN=1
            shift 1;;
         h) usage
            exit 0 ;;
@@ -26,10 +30,11 @@ do
     esac
 done
 
-if (( $COMPILE == 0 && $UPLOAD == 0 ))
+if (( $COMPILE == 0 && $UPLOAD == 0 && $SCREEN == 0 ))
 then
     COMPILE=1
     UPLOAD=1
+    SCREEN=1
 fi
 
 if (( $COMPILE == 1 ))
@@ -39,5 +44,10 @@ fi
 
 if (( $UPLOAD == 1 ))
 then
-    arduino-cli upload -v -p /dev/ttyUSB0 -b arduino:avr:nano:cpu=atmega328old -t "$1" && screen /dev/ttyUSB0 9600 || exit 2
+    arduino-cli upload -v -p /dev/ttyUSB0 -b arduino:avr:nano:cpu=atmega328old -t "$1" || exit 2
+fi
+
+if (( $SCREEN == 1 ))
+then
+    screen /dev/ttyUSB0 9600
 fi
